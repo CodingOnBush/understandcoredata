fdc17812c463e1272498222c517f8c7474f15d1f
Mostafa <mostafa.omrane@proton.me>
2023-05-24 14:21:27 +0200

    Initial Commit

---
+++ understandcoredata/understandcoredataApp.swift
@@ -0,0 +1,17 @@
+//
+//  understandcoredataApp.swift
+//  understandcoredata
+//
+//  Created by VegaPunk on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct understandcoredataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
